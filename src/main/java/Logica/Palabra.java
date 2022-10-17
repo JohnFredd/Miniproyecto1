2ba229b55b9f022cf02c91dfd094c9781e489a12
John Freddy <john.freddy.belalcazar@correounivalle.edu.co>
2022-10-16 22:58:53 -0500

    Se me olvido guardar XD

--- src/main/java/Logica/Palabra.java
+++ src/main/java/Logica/Palabra.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Logica;
 
-/**
- *
- * @author John
- */
 public class Palabra {
     
+    private int intentosTotales;
+    private int intentosEjecutados;
+    private char[] palabraSecreta;
+    private char[] miPalabra;
+    private boolean aciertaPalabra;
+    private int numeroPalabra;
+    
+    public Palabra(int numeroPalabra, Juego juego) {
+        intentosTotales = 10;
+        intentosEjecutados = 0;
+        palabraSecreta = juego.getPalabrasVsTematica()[numeroPalabra][0].toCharArray();
+        char miPalabra[] = new char[palabraSecreta.length];
+        for (int i = 0;i<miPalabra.length;i++) {
+            miPalabra[i] = '_';
+        }
+        aciertaPalabra = false;
+        this.numeroPalabra = numeroPalabra;
+    }
+    
+    public boolean evaluarIntento(char letraIngresada) {
+        boolean cambios = false;
+        for (int i = 0;i<palabraSecreta.length;i++) {
+            if (letraIngresada == palabraSecreta[i]) {
+                miPalabra[i] = palabraSecreta[i];
+                cambios = true;
+            }
+        }
+        return cambios;
+    }
+    
+    public boolean acerto() {
+        return palabraSecreta == miPalabra;
+    }
 }
