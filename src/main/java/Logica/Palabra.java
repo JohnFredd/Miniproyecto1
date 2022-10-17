e45403857759342a34ec62687dfebe1775e454c4
John Freddy <john.freddy.belalcazar@correounivalle.edu.co>
2022-10-17 15:32:12 -0500

    Restauración de palabra

--- src/main/java/Logica/Palabra.java
+++ src/main/java/Logica/Palabra.java
@@ -1,12 +1,6 @@
 
 package Logica;
 
-import Ventana.ResultadosRondaGUI;
-import java.awt.Image;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
 public class Palabra {
     private int intentosTotales;
     private int intentosEjecutados;
@@ -14,11 +8,8 @@ public class Palabra {
     private char[] miPalabra;
     private boolean aciertaPalabra;
     private int numeroPalabra;
-    private JLabel lblImagen; //
-    private Ronda ronda;  //
-    private Juego juego;  //
     
-    public Palabra(int numeroPalabra, Juego juego) {  //Palabra(int numeroPalabra, Juego juego, lblImagen, ronda(?
+    public Palabra(int numeroPalabra, Juego juego) {
    
         intentosTotales = 10;
         intentosEjecutados = 0;
@@ -29,12 +20,9 @@ public class Palabra {
         }
         aciertaPalabra = false;
         this.numeroPalabra = numeroPalabra;
-        //this.lblImagen = lblImagen;
-        //ImageIcon imagenes = new ImageIcon("/RecursosImages/1.png");
-        //lblImagen.setIcon(new ImageIcon(imagenes.getImage().getScaledInstance(lblImagen.getWidth(), lblImagen.getHeight(), Image.SCALE_SMOOTH)));
-
+        
     }
-    /*
+    
     public boolean evaluarIntento(char letraIngresada) {
         boolean cambios = false;
         for (int i = 0;i<palabraSecreta.length;i++) {
@@ -43,55 +31,11 @@ public class Palabra {
                 cambios = true;
             } else {
                 intentosEjecutados +=1;
-                //actualizar interfaz
-                ImageIcon imagenes = new ImageIcon("/RecursosImages/"+this.intentosEjecutados+".png");
-                lblImagen.setIcon(new ImageIcon(imagenes.getImage().getScaledInstance(lblImagen.getWidth(), lblImagen.getHeight(), Image.SCALE_SMOOTH)));
-            } 
-        }
-        return cambios;
-    }
-    */
-    
-    public boolean evaluarIntento(char letraIngresada) {
-        boolean cambios = false;
-        if (intentosEjecutados == intentosTotales){
-            mostrarAdNoAdivinoPalabra();
-        
-        }
-        
-        for (int i = 0;i<palabraSecreta.length;i++) {
-            if (letraIngresada == palabraSecreta[i]) {
-                miPalabra[i] = palabraSecreta[i];
-                cambios = true;
-            } else {
-                intentosEjecutados +=1;
-                //actualizar interfaz
-                ImageIcon imagenes = new ImageIcon("/RecursosImages/"+this.intentosEjecutados+".png");
-                lblImagen.setIcon(new ImageIcon(imagenes.getImage().getScaledInstance(lblImagen.getWidth(), lblImagen.getHeight(), Image.SCALE_SMOOTH)));
             } 
         }
         return cambios;
     }
 
-    public void mostrarAdNoAdivinoPalabra(){
-        //int palabrasTotales = Integer.parseInt(txtPalPorRonda.getText());                                     //ronda.getPalabrasTotales()
-       
-        int resp = JOptionPane.showConfirmDialog(null, "¡¡¡Estás ahorcado!!!. \nNo has adivinado la palabra\nDesesas continuar con las", "Alerta!", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-        if (resp == JOptionPane.NO_OPTION){
-            irResultadosRonda();
-        }
-        if (resp == JOptionPane.YES_OPTION){
-        //
-        }
-    
-    
-    }
-    //
-    public void irResultadosRonda(){
-        ResultadosRondaGUI ventanaResulRonda = new ResultadosRondaGUI("Resultados de ronda", ronda, juego);
-        ventanaResulRonda.setVisible(true);
-        this.dispose();
-    }
     public boolean acerto() {
         return palabraSecreta == miPalabra;
     }
